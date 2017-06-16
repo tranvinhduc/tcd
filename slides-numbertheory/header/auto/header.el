864c2e5db375a5885e2fc1519384492aaff400c5
Tran Vinh Duc <tran@Trans-MacBook-Pro.local>
2017-06-16 11:53:24 +0700

    Them tai lieu

--- slides-numbertheory/header/auto/header.el
+++ slides-numbertheory/header/auto/header.el
@@ -1,9 +1,25 @@
-(TeX-add-style-hook "header"
+(TeX-add-style-hook
+ "header"
  (lambda ()
-    (TeX-run-style-hooks
-     "latex2e"
-     "beamer10"
-     "beamer"
-     "mathserif"
-     "../header/main")))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "mathserif")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../header/main"
+    "beamer"
+    "beamer10")
+   (LaTeX-add-xcolor-definecolors
+    "MyBackground"))
+ :latex)
 
